1a8a2ac6949e65ca95814b20626bdaf6bad677eb
sahil-kamaliya <kamaliyasahil8@gmail.com>
2026-04-29 22:17:24 +0530

    final correct one with all working things

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,131 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect } from "react";
-
-
 
 const App = () => {
-  const [user, setUser] = useState([]);
-  const [name, setName] = useState();
-  const [disc, setDisc] = useState();
-  const [date, setDate] = useState();
-  const [hource, setHource] = useState();
+  const [reports, setReports] = useState([]);
+  const [name, setName] = useState("");
+  const [disc, setDisc] = useState("");
+  const [date, setDate] = useState("");
+  const [hours, setHours] = useState("");
+  const [filterDate, setFilterDate] = useState("");
 
-   
-
-
+  // get all reports when page loads
   useEffect(() => {
-    axios.get(`http://localhost:5000/userget`);
-    setUser.data;
+    axios
+      .get("http://localhost:5000/userget")
+      .then((res) => setReports(res.data))
+      .catch((err) => console.log("error while getting", err));
   }, []);
 
-  return (
-    <div>
-      <input type="text" placeholder="name of employe" onChange={(e)=>setName=e.target.value} />
-      <input type="text" placeholder="discption of the work" onChange={(e)=>setDisc=e.target.value} />
-      <input type="date" onChange={(e)=>setDate=e.target.value}/>
-      <input type="text" placeholder="work hour" onChange={(e)=>setHource=e.target.value} />
-      <button>Submit</button>
+  // add new report
+  const handleSubmit = () => {
+    if (!name || !disc || !date || !hours) {
+      alert("please fill all fields");
+      return;
+    }
+    axios
+      .post("http://localhost:5000/userpost", {
+        name: name,
+        disc: disc,
+        date: date,
+        hours: hours,
+      })
+      .then((res) => {
+        setReports([...reports, res.data]);
+        setName("");
+        setDisc("");
+        setDate("");
+        setHours("");
+      })
+      .catch((err) => console.log("error while adding", err));
+  };
 
-      <table border={2}>
-         <thead>
-          <th>Name</th>
-          <th>disk</th>
-          <th>Date</th>
-          <th>hours</th>
-         </thead>
-         <tbody></tbody>
+  // filter by date
+  const handleFilter = () => {
+    if (!filterDate) {
+      axios
+        .get("http://localhost:5000/userget")
+        .then((res) => setReports(res.data));
+      return;
+    }
+    axios
+      .get(`http://localhost:5000/userget/filter?date=${filterDate}`)
+      .then((res) => setReports(res.data))
+      .catch((err) => console.log("error while filtering", err));
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Employee Daily Report</h2>
+
+      {/* form to add report */}
+      <div>
+        <input
+          type="text"
+          placeholder="name of employee"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="description of work"
+          value={disc}
+          onChange={(e) => setDisc(e.target.value)}
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="work hours"
+          value={hours}
+          onChange={(e) => setHours(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+
+      {/* filter by date */}
+      <div style={{ marginTop: "20px" }}>
+        <input
+          type="date"
+          value={filterDate}
+          onChange={(e) => setFilterDate(e.target.value)}
+        />
+        <button onClick={handleFilter}>Filter</button>
+        <button
+          onClick={() => {
+            setFilterDate("");
+            axios
+              .get("http://localhost:5000/userget")
+              .then((res) => setReports(res.data));
+          }}
+        >
+          Clear
+        </button>
+      </div>
+
+      {/* table to show reports */}
+      <table border={2} style={{ marginTop: "20px", width: "100%" }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Date</th>
+            <th>Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reports.map((report, i) => (
+            <tr key={i}>
+              <td>{report.name}</td>
+              <td>{report.disc}</td>
+              <td>{report.date}</td>
+              <td>{report.hours}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
